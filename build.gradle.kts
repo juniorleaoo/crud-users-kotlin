aa1f149c558a82e88ae0b4982b44b3603ca74af4
juniorleaoo <juniorleaoo@hotmail.com>
2023-12-15 22:46:14 -0300

    chore: bump spring version

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ group = "com.crud"
 version = "0.0.1-SNAPSHOT"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_21
 }
 
 repositories {
@@ -37,7 +37,7 @@ dependencies {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs += "-Xjsr305=strict"
-        jvmTarget = "17"
+        jvmTarget = "21"
     }
 }
 
